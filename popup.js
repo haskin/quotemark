fcc87bc7128bb68c51e80eadf6e3f2f02aa5f638
dl <danliu277@gmail.com>
2019-10-30 13:40:17 -0400

    Change list to url

--- popup.js
+++ popup.js
@@ -5,9 +5,12 @@ chrome.storage.sync.get(null, function(items) {
     console.log("key: ", key);
     chrome.storage.sync.get([key], dataObject => {
       console.log("dataObject:", dataObject[key].pageUrl);
-      var node = document.createElement("LI"); // Create a <li> node
-      var textnode = document.createTextNode(dataObject[key].pageUrl); // Create a text node
-      node.appendChild(textnode);
+	  var node = document.createElement("LI"); // Create a <li> node
+	  var data = document.createElement("a");
+	  data.href = dataObject[key].pageUrl;
+	  data.text = dataObject[key].quote;
+	  data.target = "_blank";
+      node.appendChild(data);
       document.getElementById("items").appendChild(node); ///append Item
     });
   });
