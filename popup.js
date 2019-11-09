a7868fea415d1f51ba46d35ef60df10deb5cddf9
haskin <10580288+haskin@users.noreply.github.com>
2019-11-09 17:31:04 -0500

    Added url in popup.js and re-did css for popup.js.

--- popup.js
+++ popup.js
@@ -1,14 +1,16 @@
 function onClickHandler(event) {
   //Maybe wait until page loads somehow
-  
+
   const quote = event.target.text;
   // const pageUrl = event.target.href;
   const pageUrl = event.target.target;
   // mostRecent = quote;
-  
-  const request = {quote:event.target.text, 
-                  pageURL: event.target.target, 
-                  action:"createTab"}
+
+  const request = {
+    quote: event.target.text,
+    pageURL: event.target.target,
+    action: "createTab"
+  }
   //Send message to backgroundpage
   chrome.runtime.sendMessage(request, () => {
     //Message sent to background script
@@ -16,21 +18,27 @@ function onClickHandler(event) {
 };
 
 const QUOTEMARK_KEY = "quoteMarkKey";
-chrome.storage.sync.get("quoteMarkKey", function(object) {
+chrome.storage.sync.get("quoteMarkKey", function (object) {
   const storage = object.quoteMarkKey;
   //Reverse order to show most recent quotes
-  storage.reverse().forEach( (data) => {
-      const quoteContainer = document.createElement("LI"); // Create a <li> node
-      var quoteContent = document.createElement("a");
-      const hr = document.createElement("hr");
-      // data.href = dataObject[key].pageUrl;
-      quoteContent.text = data.quote;
-      quoteContent.onclick = onClickHandler;
-      quoteContent.className = "quoteContainer__quote";
-      // data.target = "_blank";
-      quoteContent.target = data.pageURL;
-      quoteContainer.appendChild(quoteContent);
-      quoteContainer.appendChild(hr);
-      document.getElementById("items").appendChild(quoteContainer); ///append Item
+  storage.reverse().forEach((data) => {
+    const quoteContainer = document.createElement("LI"); // Create a <li> node
+	const quoteContent = document.createElement("a");
+	quoteContent.textContent = data.quote;
+    quoteContent.onclick = onClickHandler;
+    quoteContent.className = "quoteContainer__quote";
+	const quoteURL = document.createElement("a");
+	quoteURL.className = "quoteContainer__URL"
+	quoteURL.textContent = data.pageURL;
+	// quoteURL.href= data.pageURL;
+    const hr = document.createElement("hr");
+    // data.href = dataObject[key].pageUrl;
+    
+    // data.target = "_blank";
+    quoteContent.target = data.pageURL;
+	quoteContainer.appendChild(quoteContent);
+	quoteContainer.appendChild(quoteURL);
+    quoteContainer.appendChild(hr);
+    document.getElementById("items").appendChild(quoteContainer); ///append Item
   });
 });
