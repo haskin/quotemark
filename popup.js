73c78475be54968f65dea70f6adbe39a5176f87e
haskin <10580288+haskin@users.noreply.github.com>
2019-11-09 18:11:19 -0500

    Added delete button to popup.js.

--- popup.js
+++ popup.js
@@ -1,3 +1,54 @@
+const QUOTEMARK_KEY = "quoteMarkKey";
+function setPopUp() {
+	/* Deletes the quoteContainer that has all copies. This is done to 
+   * refresh the popup when one of the copies is deleted. quoteContainer
+   * is added back once the storage is imported.*/
+	const tempContainer = document.querySelector(".quotesContainer");
+	if(tempContainer)
+		tempContainer.remove();
+	//New Quotes Container
+	const quotesContainer = document.createElement("DIV");
+	quotesContainer.className = "quotesContainer";
+	document.getElementById("mainBody").appendChild(quotesContainer);
+
+	chrome.storage.sync.get("quoteMarkKey", function (object) {
+		const storage = object.quoteMarkKey;
+		//Reverse order to show most recent quotes
+		storage.reverse().forEach((data) => {
+			//Quote Container
+			const quoteContainer = document.createElement("LI"); // Create a <li> node
+			quoteContainer.className = "quoteContainer"
+			//Quote Content
+			const quoteContent = document.createElement("a");
+			quoteContent.textContent = data.quote;
+			quoteContent.onclick = onClickHandler;
+			quoteContent.className = "quoteContainer__quote";
+			//Quote URL
+			const quoteURL = document.createElement("a");
+			quoteURL.className = "quoteContainer__URL"
+			quoteURL.textContent = data.pageURL;
+
+			//Delete Button
+			const deleteButton = document.createElement("button");
+			deleteButton.className = "quoteContainer__delete";
+			deleteButton.textContent = "Delete";
+			deleteButton.onclick = deleteButtonOnClickHandler;
+
+			// quoteURL.href= data.pageURL;
+			const hr = document.createElement("hr");
+			// data.href = dataObject[key].pageUrl;
+			
+			// data.target = "_blank";
+			quoteContent.target = data.pageURL;
+			quoteContainer.appendChild(quoteContent);
+			quoteContainer.appendChild(quoteURL);
+			quoteContainer.appendChild(deleteButton);
+			quoteContainer.appendChild(hr);
+			document.querySelector(".quotesContainer").appendChild(quoteContainer); ///append Item
+		});
+	});
+}
+
 function onClickHandler(event) {
   //Maybe wait until page loads somehow
 
@@ -17,28 +68,20 @@ function onClickHandler(event) {
   });
 };
 
-const QUOTEMARK_KEY = "quoteMarkKey";
-chrome.storage.sync.get("quoteMarkKey", function (object) {
-  const storage = object.quoteMarkKey;
-  //Reverse order to show most recent quotes
-  storage.reverse().forEach((data) => {
-    const quoteContainer = document.createElement("LI"); // Create a <li> node
-	const quoteContent = document.createElement("a");
-	quoteContent.textContent = data.quote;
-    quoteContent.onclick = onClickHandler;
-    quoteContent.className = "quoteContainer__quote";
-	const quoteURL = document.createElement("a");
-	quoteURL.className = "quoteContainer__URL"
-	quoteURL.textContent = data.pageURL;
-	// quoteURL.href= data.pageURL;
-    const hr = document.createElement("hr");
-    // data.href = dataObject[key].pageUrl;
-    
-    // data.target = "_blank";
-    quoteContent.target = data.pageURL;
-	quoteContainer.appendChild(quoteContent);
-	quoteContainer.appendChild(quoteURL);
-    quoteContainer.appendChild(hr);
-    document.getElementById("items").appendChild(quoteContainer); ///append Item
-  });
-});
+function deleteButtonOnClickHandler(event) {
+	// console.log(event);
+	const deleteIndex = event.path[1].childNodes[0].tabIndex;
+	chrome.storage.sync.get("quoteMarkKey", function (object) {
+		const storage = object.quoteMarkKey;
+		  //Reverse order to show most recent quotes
+		const updatedStorage = storage.reverse().filter((data, index) => 
+												index != deleteIndex)
+		chrome.storage.sync.set({"quoteMarkKey": updatedStorage}, () => {
+			console.log("Quote deleted in storage");
+			setPopUp();
+		});
+	});
+}
+
+
+setPopUp();
